024985ede65da52435bf2dca1dfd3b6929f35651
Lucas Vladson C. de Lima <lucasvladsonhd@gmail.com>
2025-08-05 18:03:40 -0300

    teste2

--- Sistemas Alunos/main.c
+++ Sistemas Alunos/main.c
@@ -4,10 +4,16 @@
 int main()
 {
     int idade;
-    char turma, curso, nome[50];
+    char turma, curso[50], nome[50];
 
     printf("Escreva seu nome: \n");
-    scanf("$s", nome);
-    printf("%s", nome);
+    fgets(&nome, 50, stdin);
+    printf("Digite sua idade: \n");
+    scanf("%d", &idade);
+    printf("Digite seu curso: \n");
+    fgets(&curso,50,stdin);
+    printf("E por ultimo, digite sua turma: \n");
+    scanf("%c", &turma);
+    printf("Seu Nome é ",nome, " Sua idade é ", idade, " Seu curso ", curso, " Da turma ", turma);
     return 0;
 }
