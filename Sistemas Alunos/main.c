518995a2390e46e43328def026dd154af6a5ef59
Lucas Vladson C. de Lima <lucasvladsonhd@gmail.com>
2025-08-08 11:12:20 -0300

    Commit final

--- Sistemas Alunos/main.c
+++ Sistemas Alunos/main.c
@@ -10,8 +10,9 @@ int main()
     fgets(nome, 50, stdin);
     printf("Digite sua idade: \n");
     scanf(" %i", &idade);
+    getchar(); // Tirar o \n do Buffer, que por algum  motivo pulava o fgets do curso
     printf("Digite seu curso: \n");
-    scanf(" %s", &curso);
+    fgets(curso, 50, stdin);
     printf("E por ultimo, digite sua turma: \n");
     scanf(" %c", &turma);
     printf("Seu nome é %s, tem %i anos, seu curso é %s e sua turma é %c", nome, idade, curso, turma);
